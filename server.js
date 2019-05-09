dd90ca9350e4b00676d9750723538275e1bef1b4
rclary0209 <48450296+rclary0209@users.noreply.github.com>
2019-05-09 14:42:09 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,24 @@
+
+var express = require("express");
+var path = require("path");
+
+
+var app = express();
+var PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/other", function(req, res) {
+  res.sendFile(path.join(__dirname, "other.html"));
+});
+
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
